7d5b4026faa812bbfd223f7f154ca8c48ec20b75
Kody Guffey <69737385+kozeee@users.noreply.github.com>
2022-05-18 11:27:03 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -21,7 +21,10 @@ app.use(express.static(__dirname + "/views"));
 function addUser(memberID, chatToken, phoneNumber, password, approvedChannels){
     console.log(memberID, chatToken, phoneNumber, password, approvedChannels)
     // Storing the JSON format data in userObject
-    let userObject = JSON.parse(fs.readFileSync("users.json", 'utf-8'));
+    let userObject = JSON.parse(fs.readFileSync("src/data/users.json", 'utf-8'));
+
+    // Backing up the most recent instance of users.json
+    fs.writeFileSync("src/data/backups/usersBackup.json",JSON.stringify(userObject));
 
     // Defining new data to be added
     let newUser = {
@@ -36,11 +39,46 @@ function addUser(memberID, chatToken, phoneNumber, password, approvedChannels){
     userObject[newUser.memberID] = newUser
 
     // Writing to our JSON file
-    fs.writeFile("users.json", JSON.stringify(userObject), (err) => {
+    fs.writeFile("src/data/users.json", JSON.stringify(userObject), (err) => {
         if (err)
             console.log(err);
         console.log(memberID + " added");
     });
+}
+
+// handles updating channel objects
+function channelsUpdate(channelList, memid){
+
+    // Reads channels.json
+    const jsonData = fs.readFileSync("src/data/channels.json", "utf8");
+
+    // Creates a backup instance to /backups/chBackup.json
+    fs.writeFileSync("src/data/backups/chBackup.json",jsonData);
+    let channels = JSON.parse(jsonData) 
+
+    // loops over channels passed
+    for (const channel of channelList){
+        // if the channel exists in channels.json, add the member by member ID
+          if (channels.hasOwnProperty(channel)){
+              if (!channels[channel].members.includes(memid))
+              {
+                channels[channel].members.push(memid)
+              }
+          }
+          // if the channel isnt in channels.json, assume its a new channel and add it to the file with creation timestamp
+          else {
+            today = new Date().toLocaleDateString()
+            channels[channel] = 
+            {
+                  "members":[memid],
+                  "phoneNumber":"",
+                  "creationDate":today
+            }
+        }
+
+        // write to data/channels.json
+        fs.writeFileSync("src/data/channels.json",JSON.stringify(channels))   
+    }
 }
 
 // define function to create chat token
@@ -83,6 +121,9 @@ app.post("/sign_up", async (req, res) => {
 
     // create user object using default channel
     addUser(member_id, token, phone_number, password, ['Welcome to SignalWire'])
+
+    // add user to the default channel
+    channelsUpdate(['Welcome to SignalWire'],member_id)
 
     // after user registration, redirect to 'home' view which should show current channels in a sidebar and user modification options
 });
